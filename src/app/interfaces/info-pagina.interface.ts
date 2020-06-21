e62b625a3f8ac01c08cbf10693928a2b354ac47c
Bryan Cubillos <Bryant_alejandro@hotmail.com>
2020-06-21 17:30:39 -0500

    interfaz

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,11 @@
+export class InfoPagina {
+    titulo: string;
+    email: string;
+    nombre_corto: string;
+    pagina_autor: string;
+    facebook: string;
+    twitter: string;
+    intagram: string;
+    equipo_trabajo: any[];
+    tumblr: string;
+};
